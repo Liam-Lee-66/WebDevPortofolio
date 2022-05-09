1c3c87a06c5e36a0acf6268328da45810915646b
[Liam-Lee-66] <liamsjlee@gmail.com>
2022-05-09 18:10:34 -0500

    A

---
+++ assets/js/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// Source https://codepen.io/alvarotrigo/pen/KKvOGNb
+gsap.registerPlugin(ScrollTrigger);
+// REVEAL //
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 80%",
+    end: "bottom 20%",
+    // mark indicator to check when text reveal or not
+    markers: false,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeave: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { y: -100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
+
+console.log('js running');
+
+
+
+
+const arrowBtn = document.getElementById('arwbtn');
+const whoAmI = document.getElementById('btn');
+
+function goDownAuto (){
+    window.scrollTo(0, document.getElementById('dos').scrollHeight);
+}
+
+arrowBtn.onclick = function () {
+    goDownAuto();
+}
+whoAmI.onclick = function () {
+    goDownAuto();
+}
